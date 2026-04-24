50d2b2d89bbb0608965cecf0c5d0c1b7c2137050
Mei05Mei <leemeiying000@gmail.com>
2026-04-24 12:37:14 +0800

    Realigning the Code for about

--- src/main/webapp/app/about/about.component.ts
+++ src/main/webapp/app/about/about.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-// This is a simple about page that can be used to display information about the application. It is intentionally simple so it loads quickly and is easy to expand later.
 @Component({
   selector: 'jhi-about',
   templateUrl: './about.component.html',
